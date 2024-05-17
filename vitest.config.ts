776215b142124560b9e2dcf9870efb4b718a6d01
Mohamed Aljoke <mohammed.mmaljoke@gmail.com>
2024-05-17 00:10:35 -0300

    :package: chore: install vitest

---
+++ vitest.config.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+});
