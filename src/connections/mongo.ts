4276ee68758a3ef4d56f1d29b13005496f39628c
Mohamed Aljoke <mohammed.mmaljoke@gmail.com>
2024-05-17 02:21:32 -0300

    chore: add docker compose

--- src/connections/mongo.ts
+++ src/connections/mongo.ts
@@ -1,14 +1,28 @@
 import log from '@utils/logger';
 import mongoose from 'mongoose';
 
+const delay = (ms:number) => new Promise(resolve => setTimeout(resolve, ms));
+
 async function connectToMongo() {
   const dbUri = process.env.dbUri || 'mongodb://root:password@localhost:27017/';
-  try {
-    await mongoose.connect(dbUri);
-    log.info('mongoose db connected ');
-  } catch (error) {
-    process.exit(1);
+  const maxRetries = 5;
+  const retryDelay = 5000; // 5 seconds
+
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      await mongoose.connect(dbUri);
+      log.info('Mongoose DB connected');
+      return;
+    } catch (error) {
+      log.error(`Attempt ${attempt} to connect to MongoDB failed. Error: ${error.message}`);
+      if (attempt < maxRetries) {
+        log.info(`Retrying in ${retryDelay / 1000} seconds...`);
+        await delay(retryDelay);
+      } else {
+        log.error('Maximum retry attempts reached. Exiting...');
+        process.exit(1);
+      }
+    }
   }
 }
-
 export default connectToMongo;
